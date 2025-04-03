5ecdbbe85767f193631c243f3409f9416877cfdf
Breshly U. Abanid <breshlyabanid@gmail.com>
2025-04-03 22:49:26 +0800

    feat:add create venue api for super admin

--- src/main/java/com/univers/univers_backend/Service/VenueService.java
+++ src/main/java/com/univers/univers_backend/Service/VenueService.java
@@ -1,4 +1,44 @@
 package com.univers.univers_backend.Service;
 
+import com.univers.univers_backend.DTO.VenueDTO;
+import com.univers.univers_backend.Entity.User;
+import com.univers.univers_backend.Entity.Venue;
+import com.univers.univers_backend.Repository.UserRepository;
+import com.univers.univers_backend.Repository.VenueRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class VenueService {
+
+    private final VenueRepository venueRepository;
+    private final UserRepository userRepository;
+
+
+    public VenueService(VenueRepository venueRepository, UserRepository userRepository){
+        this.venueRepository = venueRepository;
+        this.userRepository = userRepository;
+    }
+    public String addVenue(VenueDTO venueDTO) {
+        Optional<Venue> existingVenue = venueRepository.findByNameIgnoreCase(venueDTO.name());
+
+        if(existingVenue.isPresent()){
+            return "Venue already exists.";
+        }
+        Venue newVenue = new Venue();
+        newVenue.setName(venueDTO.name());
+        newVenue.setLocation(venueDTO.location());
+
+        if(venueDTO.venueOwnerId() != null){
+            User venueOwner = userRepository.findById(venueDTO.venueOwnerId()).orElse(null);
+
+            if(venueOwner == null){
+                return "User not found.";
+            }
+            newVenue.setVenueOwner(venueOwner);
+        }
+        venueRepository.save(newVenue);
+        return "Venue is added successfully";
+    }
 }
