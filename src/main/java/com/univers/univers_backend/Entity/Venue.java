549e9bc8ac3bd9a577bf54fdeeefcb3b2e42b999
Breshly U. Abanid <breshlyabanid@gmail.com>
2025-04-03 22:02:52 +0800

    feat:add venue entity and set up fields

--- src/main/java/com/univers/univers_backend/Entity/Venue.java
+++ src/main/java/com/univers/univers_backend/Entity/Venue.java
@@ -1,4 +1,77 @@
 package com.univers.univers_backend.Entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "venue")
 public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String location;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_owner_id", nullable = false)
+    private User venueOwner;
+
+    @Column(updatable = false)
+    LocalDateTime createdAt;
+
+    LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Venue(Long id, String name, String location, User venueOwner) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.venueOwner = venueOwner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public User getVenueOwner() {
+        return venueOwner;
+    }
+
+    public void setVenueOwner(User venueOwner) {
+        this.venueOwner = venueOwner;
+    }
 }
