4f5608c7152b04a9f822e658884949e27f5f0c03
Breshly U. Abanid <breshlyabanid@gmail.com>
2025-04-04 12:22:57 +0800

    refactor:add the createdAt and updatedAt fields in the DepartmentDTO records

--- src/main/java/com/univers/univers_backend/DTO/DepartmentDTO.java
+++ src/main/java/com/univers/univers_backend/DTO/DepartmentDTO.java
@@ -3,10 +3,14 @@ package com.univers.univers_backend.DTO;
 import com.univers.univers_backend.Entity.User;
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.LocalDateTime;
+
 public record DepartmentDTO(
 
         @NotBlank String name,
         String description,
-        Long deptHead
-) {
+        Long deptHead,
+
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt) {
 }
