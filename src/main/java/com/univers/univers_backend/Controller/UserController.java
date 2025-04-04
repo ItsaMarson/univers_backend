cf4958e1c3eb45bd0e452c95da368b951798c37d
Breshly U. Abanid <breshlyabanid@gmail.com>
2025-04-04 19:03:01 +0800

    chore:remove forgotpassword and resetpassword in UserController(was transferred to AuthController

--- src/main/java/com/univers/univers_backend/Controller/UserController.java
+++ src/main/java/com/univers/univers_backend/Controller/UserController.java
@@ -20,27 +20,6 @@ public class UserController {
     }
 
 
-    @PostMapping("/forgot-password")
-    public ResponseEntity<String> forgotPassword(@RequestBody Map<String, String> request){
-        String email = request.get("email");
-        String responseMessge = userService.forgotPassword(email);
-
-        return ResponseEntity.ok(responseMessge);
-    }
-    @PostMapping("/reset-password")
-    public ResponseEntity<String> resetPassword(@RequestBody Map<String, String> request){
-        String email = request.get("email");
-        String newPassword = request.get("newPassword");
-
-        String responseMessage= userService.resetPassword(email, newPassword);
-
-        if("Invalid reset code".equals(responseMessage) || "Reset code has expired. Please request a new one.".equals(responseMessage)){
-            return ResponseEntity.badRequest().body(responseMessage);
-        }
-
-        return ResponseEntity.ok(responseMessage);
-    }
-
     @PatchMapping("/{userId}")
     public ResponseEntity<String> updateUser(@PathVariable Long userId, @RequestBody UserDTO userDTO){
 
