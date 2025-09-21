8082ec05839d0dcb834e52ce1c5fac95af39df03
Momoyaan <ianmathewaviso@gmail.com>
2025-09-21 18:13:43 +0800

    dotenv: read from .env file

--- src/main/java/com/univers/univers_backend/UniversBackendApplication.java
+++ src/main/java/com/univers/univers_backend/UniversBackendApplication.java
@@ -1,6 +1,7 @@
 /* (C)2025 */
 package com.univers.univers_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class UniversBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(UniversBackendApplication.class, args);
 
         System.out.println("Hello World Testing");
